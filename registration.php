23ca37f39722331747e4b9d328453611d1a4890b
Arron Moss <arron.moss@zero1.co.uk>
2024-01-17 07:27:17 +0000

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
     \Magento\Framework\Component\ComponentRegistrar::register(
         \Magento\Framework\Component\ComponentRegistrar::MODULE,
-        'Zero1_PosMsi',
+        'Zero1_OpenPosMsi',
         __DIR__
     );
